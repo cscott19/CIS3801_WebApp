9ff3b91025e709c70a20c17b2c1214f78eec505e
Charice Scott <charice.scott@smail.rasmussen.edu>
2018-10-20 20:37:34 -0500

    Mod3CourseProject
    Made some updates to the home page and created the products page

---
+++ products.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+
+  <! This section styles the page>
+  <style>
+
+  body {
+    background-image: url("design.png");
+    background-repeat;
+  }
+
+  table, th, td{
+    border: 1px solid black;
+  }
+  img {
+    display:block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  <! This section creates a photo gallery>
+
+
+  div.gallery {
+      margin: 5px;
+      border: 1px solid #ccc;
+      float: left;
+      width: 90px;
+  }
+
+
+  div.gallery img {
+      width: 50%;
+      height: auto;
+  }
+
+  div.desc {
+      padding: 15px;
+      text-align: center;
+  }
+
+  </style>
+
+
+
+  <! Adding a centered linked logo as the header >
+  <title> Charice's Creations Homepage</title>
+
+  <a href="Home.html">
+  <img src="logo.png" alt="Company Logo" style="width:20%;">
+</head>
+
+<br>
+<br>
+<br>
+
+<! Adds a navigation section to the top center of the page>
+<section>
+  <span>
+    <div style="text-align:center; font-size:175%">
+    <a href="Home.html">Home</a>
+    <a href="products.php">Products</a>
+    <a href="contact.html">Contact</a>
+    <a href="qa.html">Q/A</a>
+    <a href="gallery.html">Gallery</a>
+    <a href="socialmedia.html">Social Media</a>
+  </span>
+</section>
+
+<br>
+<br>
+<br>
+
+
+<! This is the title of the column>
+    <h1 style="text-align:center; font-size:60px;">Products</h1>
+
+
+    <!Displays the database information>
+    <center>
+    <?php
+    $servername = "localhost:3306";
+    $username = "scott";
+    $password = "Sickandtired1!";
+    $dbname = "demo";
+
+    // Create a connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT Product_Name, Product_Price, Product_ID, Product_Inventory, Product_Backorder FROM test";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+
+      echo "<table><tr><th>Product</th><th>ID</th><th>Price</th><th>Current Inventory</th><th>Current Orders</th></tr>";
+
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["Product_Name"]. "</td> <td>" . $row["Product_ID"]."</td> <td>" .$row["Product_Price"]."</td> <td>" .$row["Product_Inventory"]."</td> <td>" .$row["Product_Backorder"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+    ?>
+  </center>
+
+  <style>
+
+  </style>
+  </head>
+
+  <br>
+  <br>
+  <br>
+
+  <body>
+
+  <div class="gallery">
+    <a target="_blank" href="scarf.jpg">
+      <img src="scarf.jpg" alt="Snake Scarf" width="310" height="455">
+    </a>
+    <div class="desc"> Snake scarves as well as other variations </div>
+  </div>
+
+  <div class="gallery">
+    <a target="_blank" href="coaster.jpg">
+      <img src="coaster.jpg" alt="Deadpool Coaster" width="310" height="455">
+    </a>
+    <div class="desc"> Various types of coasters </div>
+  </div>
+
+  <div class="gallery">
+    <a target="_blank" href="hat.jpg">
+      <img src="hat.jpg" alt="Camo Hat" width="310" height="455">
+    </a>
+    <div class="desc"> Hats can be made to order in any color </div>
+  </div>
+
+  <div class="gallery">
+    <a target="_blank" href="pouch.jpg">
+      <img src="pouch.jpg" alt="Pouch" width="310" height="455">
+    </a>
+    <div class="desc"> Pouches come in a variety of sizes and colors </div>
+  </div>
+</body>
+
+
+<br>
+<br>
+<br>
+
+</article>
+
+</html>
